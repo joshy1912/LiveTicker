5abe0c5938cbf06a8efca13755015a63d43ca096
joshy1912 <99416967+joshy1912@users.noreply.github.com>
2023-09-19 19:47:58 +0200

    halt der code

--- src/main/java/org/example/LiveTickerGUI.java
+++ src/main/java/org/example/LiveTickerGUI.java
@@ -4,8 +4,48 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 
 public class LiveTickerGUI extends JFrame {
+    private JLabel tickerLabel;
+    private Timer tickerTimer;
 
+    public LiveTickerGUI(){
+        setTitle("Live Ticker");
+        setSize(400, 100);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        tickerLabel = new JLabel("Live-Ticker: Warten auf Aktualiesierungen...");
+        tickerLabel.setFont(new Font("Arial", Font.BOLD, 20));
+        add(tickerLabel);
+
+        tickerTimer = new Timer(3000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Simuliere eine Aktualisierung der Daten
+                String newData = generateRanomData();
+                tickerLabel.setText("Live-Ticker"+ newData);
+
+            }
+        });
+        tickerTimer.start();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+    private String generateRanomData(){
+        Random random = new Random();
+        int scoreTeamA = random.nextInt(1);
+        int scoreTeamB = random.nextInt(1);
+        return " Leibzig " + 1 + " - young Boys " + 1;
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new LiveTickerGUI();
+            }
+        });
+    }
 }
