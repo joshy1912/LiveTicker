b8ef302d50c528acee1d1718481b023ffa819c42
joshy1912 <99416967+joshy1912@users.noreply.github.com>
2023-09-19 19:45:28 +0200

    Initial commit

---
+++ src/main/java/org/example/LiveTickerGUI.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class LiveTickerGUI extends JFrame {
+
+}
